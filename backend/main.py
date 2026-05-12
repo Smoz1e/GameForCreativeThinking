282872f19b890971110f9457a22a6e24be3f1085
Ilya <ib258650@gmail.com>
2026-05-12 13:59:56 +0500

    connaction front and backend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,14 @@
-from fastapi import FastAPI, APIRouter
+from pathlib import Path
 
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
+router = APIRouter() 
+BASE_DIR = Path(__file__).resolve().parent
+app.mount("/static", StaticFiles(directory=str(BASE_DIR.parent / "frontend")), name="static")
+templates = Jinja2Templates(directory=str(BASE_DIR.parent / "frontend" / "templates"))
+
+@app.get("/home")
+def read_root(request: Request):
+    return templates.TemplateResponse(request, "cart.html")
